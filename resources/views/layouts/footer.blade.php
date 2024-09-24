6d58c0bd37d3e8aec19f94d1672972283ad766fd
zaidsmd <zaidsmd111@gmail.com>
2024-09-24 12:00:01 +0100

    Ajouter cmi - mastercard - visa logo

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,76 +1,91 @@
 <div class="w-100 bg-orange-300 py-5 mt-auto">
-<div class="container">
-    <div class="row gap-md-0 gap-3">
-        <div class="col-md-4 text-center">
-            <a class="d-block" href="{{route('home')}}">
-                <img src="{{asset('images/logo-white.png')}}" style="max-width: 150px" class="img-fluid" alt="">
+    <div class="container">
+        <div class="row gap-md-0 gap-3">
+            <div class="col-md-4 text-center">
+                <a class="d-block" href="{{route('home')}}">
+                    <img src="{{asset('images/logo-white.png')}}" style="max-width: 150px" class="img-fluid" alt="">
 
-            </a>
-            <p class="text-white" >
-                مكتبة بوكادوبوكس لكتب الأطفال و الناشئة بالمغرب
-            </p>
-        </div>
-        <div class="col-md-4 text-center">
-            <h5 class="text-white" >التصنيفات</h5>
-            <a href="{{route('shop.tags','0-3-سنوات')}}" class="text-decoration-none">
-                <h6 class="m-0 py-2 text-white text-decoration-none">
-                    3-0 سنوات
-                </h6>
-            </a>
-            <a href="{{route('shop.tags','3-6-سنوات')}}" class="text-decoration-none">
-                <h6 class="m-0 py-2 text-white text-decoration-none">
-                    6-3 سنوات
-                </h6>
-            </a>
-            <a href="{{route('shop.tags','6-9-سنوات')}}" class="text-decoration-none">
-                <h6 class="m-0 py-2 text-white text-decoration-none">
-                    9-6 سنوات
-                </h6>
-            </a>
-            <a href="{{route('shop.tags','9-12-سنوات')}}" class="text-decoration-none">
-                <h6 class="m-0 py-2 text-white text-decoration-none">
-                    12-9 سنوات
-                </h6>
-            </a>
-            <a href="{{route('shop.tags','12-15-سنوات')}}" class="text-decoration-none">
-                <h6 class="m-0 py-2 text-white text-decoration-none">
-                    15-12 سنوات
-                </h6>
-            </a>
-            <a href="{{route('shop.tags','16-سنوات')}}" class="text-decoration-none">
-                <h6 class="m-0 py-2 text-white text-decoration-none">
-                    16+ سنوات
-                </h6>
-            </a>
-            <a href="{{route('shop.tags','الوالدية')}}" class="text-decoration-none">
-                <h6 class="m-0 py-2 text-white text-decoration-none">
-                    الوالدية
-                </h6>
-            </a>
-        </div>
-        <div class="col-md-4 text-center">
-            <h5 class="text-white" >روابط مهمة</h5>
-            <a href="{{route('static-pages.about-us')}}" class="text-decoration-none">
-                <h6 class="m-0 py-2 text-white text-decoration-none">
-                   من نحن
-                </h6>
-            </a>
-            <a href="{{route('static-pages.terms-and-conditions')}}" class="text-decoration-none">
-                <h6 class="m-0 py-2 text-white text-decoration-none">
-                   الشروط و الأحكام
-                </h6>
-            </a>
-            <a href="https://www.instagram.com/bokadobox/" target="_blank" class="text-decoration-none">
-                <h6 class="m-0 py-2 text-white text-decoration-none">
-                    صفحتنا على الانستغرام
-                </h6>
-            </a>
-            <a href="https://web.facebook.com/bokadobox?_rdc=1&_rdr" target="_blank" class="text-decoration-none">
-                <h6 class="m-0 py-2 text-white text-decoration-none">
-                    صفحتنا على الفيسبوك
-                </h6>
-            </a>
+                </a>
+                <p class="text-white">
+                    مكتبة بوكادوبوكس لكتب الأطفال و الناشئة بالمغرب
+                </p>
+            </div>
+            <div class="col-md-4 text-center">
+                <h5 class="text-white">التصنيفات</h5>
+                <a href="{{route('shop.tags','0-3-سنوات')}}" class="text-decoration-none">
+                    <h6 class="m-0 py-2 text-white text-decoration-none">
+                        3-0 سنوات
+                    </h6>
+                </a>
+                <a href="{{route('shop.tags','3-6-سنوات')}}" class="text-decoration-none">
+                    <h6 class="m-0 py-2 text-white text-decoration-none">
+                        6-3 سنوات
+                    </h6>
+                </a>
+                <a href="{{route('shop.tags','6-9-سنوات')}}" class="text-decoration-none">
+                    <h6 class="m-0 py-2 text-white text-decoration-none">
+                        9-6 سنوات
+                    </h6>
+                </a>
+                <a href="{{route('shop.tags','9-12-سنوات')}}" class="text-decoration-none">
+                    <h6 class="m-0 py-2 text-white text-decoration-none">
+                        12-9 سنوات
+                    </h6>
+                </a>
+                <a href="{{route('shop.tags','12-15-سنوات')}}" class="text-decoration-none">
+                    <h6 class="m-0 py-2 text-white text-decoration-none">
+                        15-12 سنوات
+                    </h6>
+                </a>
+                <a href="{{route('shop.tags','16-سنوات')}}" class="text-decoration-none">
+                    <h6 class="m-0 py-2 text-white text-decoration-none">
+                        16+ سنوات
+                    </h6>
+                </a>
+                <a href="{{route('shop.tags','الوالدية')}}" class="text-decoration-none">
+                    <h6 class="m-0 py-2 text-white text-decoration-none">
+                        الوالدية
+                    </h6>
+                </a>
+            </div>
+            <div class="col-md-4 text-center">
+                <h5 class="text-white">روابط مهمة</h5>
+                <a href="{{route('static-pages.about-us')}}" class="text-decoration-none">
+                    <h6 class="m-0 py-2 text-white text-decoration-none">
+                        من نحن
+                    </h6>
+                </a>
+                <a href="{{route('static-pages.terms-and-conditions')}}" class="text-decoration-none">
+                    <h6 class="m-0 py-2 text-white text-decoration-none">
+                        الشروط و الأحكام
+                    </h6>
+                </a>
+                <a href="https://www.instagram.com/bokadobox/" target="_blank" class="text-decoration-none">
+                    <h6 class="m-0 py-2 text-white text-decoration-none">
+                        صفحتنا على الانستغرام
+                    </h6>
+                </a>
+                <a href="https://web.facebook.com/bokadobox?_rdc=1&_rdr" target="_blank" class="text-decoration-none">
+                    <h6 class="m-0 py-2 text-white text-decoration-none">
+                        صفحتنا على الفيسبوك
+                    </h6>
+                </a>
+                <div class="row align-items-center  mt-4">
+                    <div class="col-3">
+                        <img src="{{asset('images/securecode.png')}}" class="img-fluid" alt="">
+                    </div>
+                    <div class="col-3">
+                        <img src="{{asset('images/verified-by-visa-logo-825D2E4C69-seeklogo.com.png')}}"
+                             class="img-fluid" alt="">
+                    </div>
+                    <div class=" col-3 ">
+                        <img src="{{asset('images/Asset-1@4x.webp')}}" class="img-fluid" alt="">
+                    </div>
+                    <div class=" col-3">
+                        <img src="{{asset('images/logo-cmi-open.png')}}" class="img-fluid" alt="">
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
-</div>
 </div>
